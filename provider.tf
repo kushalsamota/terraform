4a51569f402a8cd40c76c89d542d95037d49afde
kushalsamota <kushalsamota@gmail.com>
2025-09-27 12:50:26 +0530

    abc

--- provider.tf
+++ provider.tf
@@ -1,3 +1,12 @@
 provider "aws" {
   region     = "ap-south-1"
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "kushal123456789"
+    key = "terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "s3-state-lock1"
+  }
+}
