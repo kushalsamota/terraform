f5353aac148d80051b3b20b38e4935a7a78ce43f
Kushal Samota <kushalsamota@gmail.com>
2025-10-05 12:53:54 +0530

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -2,6 +2,6 @@ resource "aws_instance" "webserver" {
     ami = "ami-01b6d88af12965bb6"
     instance_type = "t2.micro"
     tags = {
-        "Name" = "dbserver"
+        "Name" = "webserver"
     }
 }
